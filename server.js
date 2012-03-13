2ba03cbb69ccc98dbbd7ea5275e3b27c4ed8d104
pochi.black <pochi.black@gmail.com>
2012-03-13 15:43:42 +0900

    Refactoring

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require("http");
 
-http.createServer(function(request, response){
+function onRequest(request, response){
   response.writeHead(200, {"Content-Type":"text/plain"});
   response.write("Hello World");
   response.end();
-}).listen(8888);
+};
+
+http.createServer(onRequest).listen(8888);
