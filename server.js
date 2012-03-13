4a42d9bc9269eadd4829b333bb594ae3e397967c
pochi.black <pochi.black@gmail.com>
2012-03-13 16:15:27 +0900

    Create router

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
   function onRequest(request, response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
     console.log("Request received.");
     response.writeHead(200, {"Content-Type":"text/plain"});
     response.write("Hello World");
