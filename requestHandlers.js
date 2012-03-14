4b8698ad91b21f58df865a57024dc8ab16cd3c73
pochi.black <pochi.black@gmail.com>
2012-03-14 19:53:37 +0900

    Add querystring

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,7 @@
 var exec = require("child_process").exec;
+var querystring = require("querystring");
 
-function start(response) {
+function start(response, postData) {
   console.log("Request handler 'start' was called");
   var content = "empty";
 
@@ -21,10 +22,10 @@ function start(response) {
     response.end();
   }
 
-function upload(response) {
+function upload(response, postData) {
   console.log("Request handler 'upload' was called");
   response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("Hello Upload");
+  response.write("You've sent:" + querystring.parse(postData).text);
   response.end();
 }
 
--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-function route(handle, pathname, response){
+function route(handle, pathname, response, postData){
   console.log("Abount to route a request for " + pathname);
 
   if(typeof handle[pathname]==="function"){
-    return handle[pathname](response);
+    return handle[pathname](response, postData);
   } else {
     console.log("No request handler found for" + pathname);
     response.writeHead(404, {"Content-Type":"text/plain"});
