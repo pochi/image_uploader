2902a2f70f0cedc70d734391e31413d9985d7645
pochi.black <pochi.black@gmail.com>
2012-03-14 18:14:04 +0900

    Use async method

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
-function route(handle, pathname){
+function route(handle, pathname, response){
   console.log("Abount to route a request for " + pathname);
 
   if(typeof handle[pathname]==="function"){
-    return handle[pathname]();
+    return handle[pathname](response);
   } else {
     console.log("No request handler found for" + pathname);
-    return "404 Not found";
+    response.writeHead(404, {"Content-Type":"text/plain"});
+    response.write("404 NotFound");
+    response.end();
   }
 }
 
