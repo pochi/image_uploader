d9cd22c09fa55b5d663747ff99043e07e7305fc9
pochi.black <pochi.black@gmail.com>
2012-03-14 19:31:23 +0900

    Create html form

--- requestHandlers.js
+++ requestHandlers.js
@@ -4,12 +4,22 @@ function start(response) {
   console.log("Request handler 'start' was called");
   var content = "empty";
 
-  exec("ls -alh", function(error, stdout, stderr) {
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write(stdout);
+  var body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Submit text">' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write(body);
     response.end();
-  });
-}
+  }
 
 function upload(response) {
   console.log("Request handler 'upload' was called");
