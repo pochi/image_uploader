2f5ae4f887ea17cd0d1c9518e9e75512041dc367
pochi.black <pochi.black@gmail.com>
2012-03-15 13:37:23 +0900

    Complete: browser can upload image file

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,5 @@
 var exec = require("child_process").exec;
-var querystring = require("querystring"), fs = require("fs");
+var querystring = require("querystring"), fs = require("fs"), formidable = require("formidable");
 
 function start(response, postData) {
   console.log("Request handler 'start' was called");
@@ -10,9 +10,9 @@ function start(response, postData) {
     '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">' +
     '</head>' +
     '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text">' +
+    '<form action="/upload" enctype="multipart/form-data" method="post">' +
+    '<input type="file" name="upload" multiple="multiple"></textarea>' +
+    '<input type="submit" value="Upload file">' +
     '</form>' +
     '</body>' +
     '</html>';
@@ -22,10 +22,23 @@ function start(response, postData) {
     response.end();
   }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called");
-  response.writeHead(200,{"Content-Type":"text/plain"});
-  response.write("You've sent:" + querystring.parse(postData).text);
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files){
+    console.log("parsing done");
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+      if(err) {
+        fs.unlink("/tmp/test.png");
+        fs.unlink(files.upload.path, "/tmp/test.png");
+      } else {
+      }
+    });
+  });
+  response.writeHead(200,{"Content-Type":"text/html"});
+  response.write("Received image:<br/>");
+  response.write("<img src='/show' />");
   response.end();
 }
 
